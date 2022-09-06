91babc7f072430feb26dd0e2f94d27dcb62e059b
Vinoth12345678900 <strvinoth358@gmail.com>
2022-09-06 11:33:45 +0530

    project structure

---
+++ src/test/java/org/test/ProjectClass.java
@@ -0,0 +1,10 @@
+package org.test;
+
+public class ProjectClass {
+	public static void main(String[] args) {
+		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
+	}
+
+}
